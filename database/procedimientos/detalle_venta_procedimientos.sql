53ac9475236dd1645a44228557cd8bcaf6133170
Daniel Ramirez <ramirezcdany@gmail.com>
2026-03-16 13:52:41 -0600

    Procedimiento de detalle ventas. Daniel Ramirez

---
+++ database/procedimientos/detalle_venta_procedimientos.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE PROCEDURE sp_agregar_detalle_venta(
+    p_idventa NUMBER,
+    p_idproducto NUMBER,
+    p_cantidad NUMBER,
+    p_preciounitario NUMBER,
+    p_subtotal NUMBER
+)
+AS
+BEGIN
+    INSERT INTO DETALLE_VENTA(IDVenta, IDProducto, Cantidad, PrecioUnitario, Subtotal)
+    VALUES(p_idventa, p_idproducto, p_cantidad, p_preciounitario, p_subtotal);
+END;
+/
+
+CREATE OR REPLACE PROCEDURE sp_eliminar_detalle_venta(
+    p_iddetalle NUMBER
+)
+AS
+BEGIN
+    DELETE FROM DETALLE_VENTA
+    WHERE IDDetalleVenta = p_iddetalle;
+END;
+/
